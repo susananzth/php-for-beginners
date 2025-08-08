b03fbbb6b7de62f39ee4ba6b25f485ee605b5333
Susana Piñero <susananzth.dev@gmail.com>
2025-08-08 17:50:59 -0500

    feat(funciones): Mejora en funciones

--- funciones.php
+++ funciones.php
@@ -2,7 +2,7 @@
 
 // Función para validar una url
 function urlIs($valor) {
-    return $_SERVER['REQUEST_URI'] === $valor;
+    return parse_url($_SERVER['REQUEST_URI'])['path'] === $valor;
 }
 
 ?>
